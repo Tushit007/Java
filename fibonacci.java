0a75771bf5dc982e32f85c7cee54188d91d61f02
Tushit <tushit.2001@gmail.com>
2025-01-23 19:23:54 +0530

    Fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class fibonacci {
+    public static void main(String[] args) throws  IOException {
+        BufferedReader br =new BufferedReader((new InputStreamReader(System.in)));
+        System.err.print("Enter no. of fibonnaci series: ");
+        int n =Integer.parseInt(br.readLine());
+        int f1=0,f2=1;
+        int count=0;
+        int f=0;
+        System.out.println(f);
+        while(count<n){
+            f1=f2;
+            f2=f;
+            f=f1+f2;
+            System.out.println(f);
+            count++;
+        }
+    }
+}
